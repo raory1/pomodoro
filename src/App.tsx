b4e38e4964e9f8bc57f1239dc87eef7f42c74447
Isabella <isa021andrade@gmail.com>
2025-07-27 10:37:59 -0300

    create Heading and Container components

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,14 @@ import './styles/theme.css';
 import './styles/global.css';
 import { IconClockHour11Filled } from '@tabler/icons-react';
 import { Heading } from './components/Heading';
+import { Container } from './components/Container';
 
 export function App() {
   return (
     <>
-      <div className="container">
-        <div className="content">
-          <section>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint esse
-            aut enim perspiciatis dolorum quis omnis architecto deleniti.
-            Consequuntur non amet quidem odit, debitis perferendis? Labore
-            perspiciatis odit non exercitationem?
-          </section>
-        </div>
-      </div>
+      <Container>
+        <Heading>asd</Heading>
+      </Container>
     </>
   );
 }
