9154c6163fe373857dd0cc685d5ccdd4d7900299
Isabella <isa021andrade@gmail.com>
2025-07-27 18:28:16 -0300

    feat: create responsive CountDown component

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import './styles/global.css';
 import { Container } from './components/Container';
 import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
+import { CountDown } from './components/CountDown';
 
 export function App() {
   return (
     <>
       <Container>
-        <Logo/>
-        <Menu/>
+        <Logo />
+        <Menu />
+      </Container>
+
+      <Container>
+        <CountDown />
       </Container>
     </>
   );
---
+++ src/components/CountDown/index.tsx
@@ -0,0 +1,6 @@
+import { IconClockHour4Filled } from '@tabler/icons-react';
+import styles from './styles.module.css';
+
+export function CountDown() {
+  return <div className={styles.container}>00:00</div>;
+}
