7ab09e03c83585abc35dc3409152497beea8c3a5
Isabella <isa021andrade@gmail.com>
2025-07-28 10:36:49 -0300

    feat: add form

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { Logo } from './components/Logo';
 import { Menu } from './components/Menu';
 import { CountDown } from './components/CountDown';
 import { Input } from './components/Input';
+import { CycleTimeline } from './components/CycleTimeline';
 
 export function App() {
   return (
@@ -19,7 +20,22 @@ export function App() {
       </Container>
 
       <Container>
-        <Input id={'task'} label={'task'} required={false} />
+        <form className="form">
+          <div className="formRow">
+            <Input
+              id={'task'}
+              label={'task'}
+              required={false}
+              placeholder="Type your task"
+            />
+          </div>
+          <div className="formRow">
+            <CycleTimeline />
+          </div>
+          <div className="formRow">
+            <button>Enviar</button>
+          </div>
+        </form>
       </Container>
     </>
   );
