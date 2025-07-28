8eba83709a819e98f8c8c94b916a386046c9c50a
Isabella <isa021andrade@gmail.com>
2025-07-28 19:39:42 -0300

    add aria-label and title on menu buttons

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,16 +1,36 @@
-import { IconClockHour4Filled, IconHistory, IconHome, IconSettings } from '@tabler/icons-react';
+import {
+  IconClockHour4Filled,
+  IconHistory,
+  IconHome,
+  IconSettings,
+} from '@tabler/icons-react';
 import styles from './styles.module.css';
 
 export function Menu() {
   return (
     <nav className={styles.menu}>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Go to home page"
+        title="Home"
+      >
         <IconHome stroke={1} />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Go to settings page"
+        title="Settings"
+      >
         <IconSettings stroke={1} />
       </a>
-      <a href="#" className={styles.menuLink}>
+      <a
+        href="#"
+        className={styles.menuLink}
+        aria-label="Go to history page"
+        title="History"
+      >
         <IconHistory stroke={1} />
       </a>
       {/* <a href="#" className={styles.menuLink}>
