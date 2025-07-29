4af3519098ce320169f0f9bb8305c2de81f53aa0
Isabella <isa021andrade@gmail.com>
2025-07-28 22:13:54 -0300

    add blank target attribute to footer links

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,10 +3,13 @@ import styles from './styles.module.css';
 export function Footer() {
   return (
     <footer className={styles.footer}>
-      <a href="https://en.wikipedia.org/wiki/Pomodoro_Technique">
+      <a
+        href="https://en.wikipedia.org/wiki/Pomodoro_Technique"
+        target="_blank"
+      >
         What is the pomodoro technique?
       </a>
-      <a href="https://github.com/raory1/pomodoro">
+      <a href="https://github.com/raory1/pomodoro" target="_blank">
         pomo timer &copy; {new Date().getFullYear()}
       </a>
     </footer>
