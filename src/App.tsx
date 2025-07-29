e8d0dbb4ffba8b9be35b0d96d470d09eb61b42fc
Isabella <isa021andrade@gmail.com>
2025-07-29 08:45:36 -0300

    feat: create Home and 404 pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,54 +1,12 @@
 import './styles/theme.css';
 import './styles/global.css';
-import { Container } from './components/Container';
-import { Logo } from './components/Logo';
-import { Menu } from './components/Menu';
-import { CountDown } from './components/CountDown';
-import { Input } from './components/Input';
-import { CycleTimeline } from './components/CycleTimeline';
-import { Button } from './components/Button';
-import { IconPlayerPlay, IconPlayerStop } from '@tabler/icons-react';
-import { Footer } from './components/Footer';
-import { ToggleTheme } from './components/ToggleTheme';
+
+import { Home } from './pages/Home';
 
 export function App() {
   return (
     <>
-      <Container>
-        <ToggleTheme />
-        <Logo />
-        <Menu />
-      </Container>
-
-      <Container>
-        <CountDown />
-      </Container>
-
-      <Container>
-        <form className="form">
-          <div className="formRow">
-            <Input
-              id={'task'}
-              label={'task'}
-              required={false}
-              placeholder="Type your task"
-            />
-          </div>
-          <div className="formRow">
-            <CycleTimeline />
-          </div>
-          <div className="formRow">
-            <Button
-              title={'Start focusing'}
-              variant="default"
-              icon={<IconPlayerPlay stroke={1.5} />}
-            />
-            {/* <Button title={"Give up"} variant='alert' icon={<IconPlayerStop stroke={1.5} />} /> */}
-          </div>
-        </form>
-      </Container>
-
-      <Footer />
+      <Home/>
     </>
   );
 }
---
+++ src/pages/NotFound/index.tsx
@@ -0,0 +1,18 @@
+import { Container } from '../../components/Container';
+import { MainTemplate } from '../../templates/MainTemplate';
+
+export function NotFound() {
+  return (
+    <MainTemplate>
+      <Container>
+        <h1>404</h1>
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Enim ullam
+          illum perspiciatis delectus recusandae est exercitationem dicta et
+          aliquam ad autem voluptatum, accusantium numquam rem officia accusamus
+          doloribus fugit tenetur?r
+        </span>
+      </Container>
+    </MainTemplate>
+  );
+}
