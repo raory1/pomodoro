6bc5aa580df38704ef6dae4e6b1b94253c088da1
Isabella <isa021andrade@gmail.com>
2025-07-30 09:38:28 -0300

    create taskContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import './styles/theme.css';
 import './styles/global.css';
 
 import { Home } from './pages/Home';
+import { TaskContextProvider } from './contexts/TaskContex';
+
 
 export function App() {
   return (
-    <>
-      <Home/>
-    </>
+    <TaskContextProvider>
+      <Home />
+    </TaskContextProvider>
   );
 }
