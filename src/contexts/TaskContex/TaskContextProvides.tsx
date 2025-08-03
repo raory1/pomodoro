5d54fdc4c065c55d0819086a09fb0f2e324a4208
Isabella <isa021andrade@gmail.com>
2025-08-03 14:22:26 -0300

    feat: integrate worker with TaskContextProvider

--- src/contexts/TaskContex/TaskContextProvides.tsx
+++ src/contexts/TaskContex/TaskContextProvides.tsx
@@ -1,7 +1,8 @@
-import { useReducer } from 'react';
+import { useEffect, useReducer } from 'react';
 import { initialState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
+import { TimerWorkerManager } from '../../workers/TimerWorkerManager';
 
 type TaskContextProviderProps = {
   children: React.ReactNode;
@@ -9,6 +10,28 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
   const [state, dispatch] = useReducer(taskReducer, initialState);
+
+  const worker = TimerWorkerManager.getInstance();
+
+  worker.onmessage((e) => {
+    const remainingTime = e.data;
+    console.log('Remaining Time:', remainingTime);
+
+    if (remainingTime <= 0) {
+      worker.terminate();
+      console.log('Worker done processing');
+    }
+  });
+
+  useEffect(() => {
+    if (!state.activeTask) {
+      console.log('Worker finished no active task');
+      worker.terminate();
+    } else {
+      worker.postMessage(state);
+    }
+  }, [worker, state]);
+
   return (
     <TaskContext.Provider value={{ state, dispatch }}>
       {children}
